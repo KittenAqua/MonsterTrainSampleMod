6ee87d1f40560fca1b23158420ca170c51c332ad
KittenAqua <mnpntgt@gmail.com>
2020-05-31 14:21:31 -0500

    Adjust custom card example patch to use new API functionality

--- MonsterTrainTestMod/SamplePatches/CustomCardExamplePatch.cs
+++ MonsterTrainTestMod/SamplePatches/CustomCardExamplePatch.cs
@@ -3,20 +3,49 @@ using System.Collections.Generic;
 using System.Text;
 using HarmonyLib;
 using MonsterTrainModdingAPI.Builder;
+using MonsterTrainModdingAPI.Managers;
 using MonsterTrainModdingAPI.Enum;
 
 namespace MonsterTrainTestMod.SamplePatches
 {
+    [HarmonyPatch(typeof(SaveManager), "Initialize")]
+    class RegisterNotHornBreak
+    {
+        // Creates a 2-cost, 12 damage spell named "Not Horn Break" with Flash Freeze's art
+        static void Postfix(ref SaveManager __instance)
+        {
+            AllGameData allGameData = __instance.GetAllGameData();
+            NotHornBreakDataCreator.CreateCardData(allGameData);
+        }
+    }
+
+    [HarmonyPatch(typeof(SaveManager), "SetupRun")]
+    class AddNotHornBreakToStartingDeck
+    {
+        // Adds Not Horn Break to the player's starting deck if the primary class is Awoken.
+        static void Postfix(ref SaveManager __instance)
+        {
+            if (CustomCardManager.CurrentPrimaryClan().GetTitle() == "Awoken")
+            {
+                var notHornBreakCardData = CustomCardManager.GetCardDataByID("NotHornBreak");
+                __instance.AddCardToDeck(notHornBreakCardData, null, false, false, false, true, true);
+            }
+        }
+    }
+
     class NotHornBreakDataCreator
     {
-        public static CardData CreateCardData(AllGameData allGameData)
+        public static void CreateCardData(AllGameData allGameData)
         {
-            CardDataBuilder cardDataBuilder = new CardDataBuilder();
-            cardDataBuilder.Cost = 2;
-            cardDataBuilder.Name = "Not Horn Break";
-            cardDataBuilder.OverrideDescriptionKey = "CardData_overrideDescriptionKey-cea726817805edf9-cb7f8f1805d4cd4449185ec893355baa-v2";
-            cardDataBuilder.TargetsRoom = true;
-            cardDataBuilder.Targetless = false;
+            CardDataBuilder cardDataBuilder = new CardDataBuilder
+            {
+                CardID = "NotHornBreak",
+                Name = "Not Horn Break",
+                Cost = 2,
+                OverrideDescriptionKey = "CardData_overrideDescriptionKey-cea726817805edf9-cb7f8f1805d4cd4449185ec893355baa-v2",
+                TargetsRoom = true,
+                Targetless = false
+            };
             cardDataBuilder.CreateAndSetCardArtPrefabVariantRef(
                 "Assets/GameData/CardArt/Portrait_Prefabs/CardArt_Spell_FlashFreeze.prefab",
                 "52471f4f40ea12d4a9a80a91f211fd07"
@@ -34,22 +63,7 @@ namespace MonsterTrainTestMod.SamplePatches
                 .SetValue(damageEffect, new String[0]);
             cardDataBuilder.Effects.Add(damageEffect);
 
-            CardData cardData = cardDataBuilder.BuildAndRegister(allGameData);
-
-            allGameData.GetAllCardData().Add(cardData);
-
-            return cardData;
-        }
-    }
-
-    [HarmonyPatch(typeof(SaveManager), "SetupRun")]
-    class AddNotHornBreakToStartingDeck
-    {
-        // Adds a 2-cost, 12 damage spell named "Not Horn Break" with Flash Freeze's art to the player's starting deck if the primary class is Awoken.
-        static void Postfix(ref SaveManager __instance, ref AllGameData ___allGameData)
-        {
-            CardData notHornBreakCardData = NotHornBreakDataCreator.CreateCardData(___allGameData);
-            __instance.AddCardToDeck(notHornBreakCardData, null, false, false, false, true, true);
+            cardDataBuilder.BuildAndRegister();
         }
     }
 }
